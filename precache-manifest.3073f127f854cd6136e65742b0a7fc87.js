3f4b6adff950f5b3ac6c1f64de6be9173ea840e5
Cody Reed <cody2j2reed@gmail.com>
2018-10-22 23:36:55 -0400

    Updates

---
+++ precache-manifest.3073f127f854cd6136e65742b0a7fc87.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5cf96c3df901918e3cde859526feb9",
+    "url": "/spi_dev/static/media/img-continuous-improvement-loop.4e5cf96c.svg"
+  },
+  {
+    "revision": "7b40094113d02573a8c7ec9eb06e374b",
+    "url": "/spi_dev/static/media/img-bermuda-sales-triangle.7b400941.svg"
+  },
+  {
+    "revision": "e4c27bd11d6f7f8a2d39b1443ee3de05",
+    "url": "/spi_dev/static/media/icn-spi.e4c27bd1.svg"
+  },
+  {
+    "revision": "c2e1a82ddf1d2c8c41eb1e19bcc08a42",
+    "url": "/spi_dev/static/media/icn-logo.c2e1a82d.svg"
+  },
+  {
+    "revision": "8ebf9d556ecb26d06268",
+    "url": "/spi_dev/static/js/runtime~main.8ebf9d55.js"
+  },
+  {
+    "revision": "c2c448cf2848c3aba065",
+    "url": "/spi_dev/static/js/main.c2c448cf.chunk.js"
+  },
+  {
+    "revision": "845b23aea6178c6dc9d9",
+    "url": "/spi_dev/static/js/1.845b23ae.chunk.js"
+  },
+  {
+    "revision": "c2c448cf2848c3aba065",
+    "url": "/spi_dev/static/css/main.6cb22e1b.chunk.css"
+  },
+  {
+    "revision": "f9f0174bb6f7f52c266aca389b7c1fb5",
+    "url": "/spi_dev/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/spi_dev/precache-manifest.3073f127f854cd6136e65742b0a7fc87.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/spi_dev/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
